0d57e6c36044513bcd462e50ed2afd37989f95e3
rohit <rohitjames77@outlook.com>
2024-09-12 09:44:02 +0530

    Imported Destructured data

--- src/displayData.js
+++ src/displayData.js
@@ -1,4 +1,24 @@
-import { tester } from "./inputAPI";
+import { destructureData } from "./inputAPI";
 
-let testing = tester();
-console.log(testing);
+let minTemp = document.querySelector('.min-temp');
+let maxTemp = document.querySelector('.max-temp');
+let sunrise= document.querySelector('.sunrise');
+let sunset = document.querySelector('.sunset');
+let humidity= document.querySelector('.humidity');
+let condition = document.querySelector('.condition');
+let fellLike = document.querySelector('.feels-like');
+let weatherIcon = document.querySelector('.weather-icon');
+
+
+async function displayData(){
+  try {
+    const data = await destructureData();
+    console.log(data);
+    return data ;
+  } catch (error) {
+    console.error("Error in DisplayData:", error);
+  }
+}
+displayData();
+
+export{displayData}
